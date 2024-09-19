f371042085c769a44e6585a65c78436156d49e78
lastieee <lastieee@o2.pl>
2024-09-19 10:21:22 +0200

    Dodano funkcję sprawdzająca czy liczba jest pierwsza

--- projekt1.cpp
+++ projekt1.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+include <iostream>
 
 using namespace std;
+bool czyPierwsza(int n){
+	if (n <= 1) return false;
+	for (int i = 2; i * i <= n; i++) {
+		if (n % i == 0) return false;
+	}
+	return true;
+
+}
+
 
 int main() {
     int a, b;
